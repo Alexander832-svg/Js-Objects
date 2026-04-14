393a596b256914abe74b7f632ff3e44b21e453a3
Alexander832-svg <chandervaldez@gmail.com>
2026-04-14 18:33:48 +0000

    progress

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 console.log("Script started")
 
 // TODO: Test working with objects here
+let person = {
+    name: "Paul",
+    age: "80",
+    eyeColor: "green",
+    ssn: "456-33-1989"
+}
+console.log(person.name);
 
+person.name = "Ringo";
+console.log(person.name);
+
+person.birthday = "Jan 1";
+console.log(person);
 
 
 function processForm() {
@@ -9,6 +21,12 @@ function processForm() {
     // TODO: prevent this function from reloading the page when the form is submitted
 
     // TODO: Create a newUser object that has all the user's info from the form
+    let newUser = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        birthdate: document.getElementById("birthdate").value,
+        favColor: document.getElementById("favColor").value
+    }
 
     // TODO: Call the addUser function and pass the newUser object as a parameter
 }
